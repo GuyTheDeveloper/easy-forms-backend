5daf779985177c9dbeab76b31114dc5a29347f62
Guy <goldenrodge99@gmail.com>
2025-02-07 23:42:12 +0500

    hotfix: cors error

--- src/index.js
+++ src/index.js
@@ -17,6 +17,14 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", FRONTEND_ORIGIN);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(`/api/${API_VERSION}`, modules);
 
 app.use((err, req, res, next) => {
