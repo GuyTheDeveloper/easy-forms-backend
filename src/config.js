ef10de9dbf134f695172d89265f9fc10cffabacc
Guy <goldenrodge99@gmail.com>
2025-02-07 15:43:12 +0500

    hotfix: pg config

--- src/config.js
+++ src/config.js
@@ -6,5 +6,19 @@ const JWT_SECRET = process.env.JWT_SECRET ?? "";
 const API_VERSION = process.env.API_VERSION ?? "v1";
 const passwordKeyLen = 48;
 const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN ?? "http://localhost:3000";
+const PG_USER = process.env.PG_USER;
+const PG_HOST = process.env.PG_HOST;
+const PG_DATABASE = process.env.PG_DATABASE;
+const PG_PASSWORD = process.env.PG_PASSWORD;
 
-export { PORT, JWT_SECRET, API_VERSION, passwordKeyLen, FRONTEND_ORIGIN };
+export {
+  PORT,
+  JWT_SECRET,
+  API_VERSION,
+  passwordKeyLen,
+  FRONTEND_ORIGIN,
+  PG_USER,
+  PG_HOST,
+  PG_DATABASE,
+  PG_PASSWORD,
+};
--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -1,8 +1,9 @@
 // const { Sequelize } = require("sequelize");
 import { Sequelize } from "sequelize";
+import { PG_DATABASE, PG_HOST, PG_PASSWORD, PG_USER } from "../config.js";
 
-const sequelize = new Sequelize("easy_forms", "guy", "peopleAreTools", {
-  host: "localhost",
+const sequelize = new Sequelize(PG_DATABASE, PG_USER, PG_PASSWORD, {
+  host: PG_HOST,
   dialect: "postgres",
   logging: false,
 });
